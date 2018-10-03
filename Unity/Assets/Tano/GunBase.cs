f70e98d6faedbc397c71fafc651cee2e66d893ba
Tano417 <renn0203@yahoo.co.jp>
2018-10-03 17:59:31 +0900

    Geometarical Dominaterから銃関連のデータを入れました

---
+++ Unity/Assets/Tano/GunBase.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBase : MonoBehaviour
+{
+
+    [SerializeField]
+    protected AudioClip shotSe;
+    [SerializeField] protected MuzzleCtrl muzleCtrl;
+    [SerializeField] protected float initialVelocity;
+    protected AudioSource audioSource;
+    [System.NonSerialized] private static bool safety = false;
+
+    //protected CharacterController rootCharacterController;
+
+    public static bool Safety
+    {
+        get
+        {
+            return safety;
+        }
+
+        set
+        {
+            safety = value;
+        }
+    }
+
+    protected void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Use this for initialization
+    protected virtual void Start()
+    {
+        muzleCtrl.initialVelocity = initialVelocity;
+    }
+
+    //will be private
+    protected virtual void Update()
+    {
+
+    }
+
+    protected void Fire()
+    {
+        if(safety)
+        {
+            return;
+        }
+
+
+        bulletShoot();
+
+        audioSource.PlayOneShot(shotSe);
+    }
+
+    protected virtual void bulletShoot()
+    {
+        muzleCtrl.Fire();
+    }
+    
+}
---
+++ Unity/Assets/Tano/MuzzleCtrl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuzzleCtrl : MonoBehaviour
+{
+    [SerializeField] GameObject bullet;
+    [SerializeField] GameObject FireParticle;
+
+    [System.NonSerialized] public float initialVelocity;
+
+    Quaternion bulletRotation = new Quaternion(0, 0, 0, 0);
+    GameObject createdBullet;
+    BulletCtrl bulletCtrl;
+
+    public void Update()
+    {
+
+    }
+
+    public void Fire()
+    {
+        //パーティクルのオブジェクト作成
+        //Instantiate(FireParticle, transform.position, transform.rotation, transform);
+
+
+        //弾を作成
+        createdBullet = Instantiate(bullet, transform.position, bulletRotation);
+        //弾にデータを与える
+        bulletCtrl = createdBullet.GetComponent<BulletCtrl>();
+        createdBullet.GetComponent<Rigidbody>().AddForce(createdBullet.transform.forward * initialVelocity, ForceMode.VelocityChange);
+    }
+
+}
