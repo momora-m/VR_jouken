8dbcd1192842716d3adbd04fe25871a9afce0ac4
DESKTOP-NUJTJD3\Jouhouken <kai10mishina@gmail.com>
2018-10-05 19:55:44 +0900

    銃の追従システムとテレポート機能の実装

---
+++ Unity/Assets/Scripts/MovingGun.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingGun : MonoBehaviour {
+
+    
+    public GameObject Player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform GunTransform = this.transform;
+        Vector3 pos = Player.transform.position;
+        pos.x += 1;
+        pos.y += 1;
+        pos.z += 1;
+        GunTransform.position = pos;
+    }
+}
